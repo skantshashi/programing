0ff921c221f424bd0004c5f27dfc786b29f586f7
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-10-18 11:06:42 +0530

    function overiding

---
+++ OPPS/Polymorphism/functionoverindng.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+//function overiding : Redfine a  fxn of a base class in derived class
+class Base
+{
+    public:
+    void display()
+    {
+        cout<<"Display Base"<<endl;
+    }
+};
+class Derived :public Base
+{
+   public:
+   void display()
+   {
+       cout<<"Display derived"<<endl;
+   } 
+};
+int main()
+{
+    Derived d;
+    d.display();
+    return 0;
+}
