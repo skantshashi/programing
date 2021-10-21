1c8a5b7d54dcb4b319cdc6ef38c1e419f3ae764e
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-10-21 23:12:15 +0530

    exception

---
+++ OPPS/exceptionahHandling/exceptional.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+using namespace std;
+    
+/*program for exception handling construct
+
+*/
+int main()
+{
+	int x=10,y=2,z;
+	try
+	{
+		if(y==0)
+			throw 1;
+		z=x/y;
+		cout<<z<<endl;
+	}
+	catch(int e)
+	{
+		cout<<"division by zero"<<e<<endl;
+	}
+	cout<<"bye"<<endl;
+}
