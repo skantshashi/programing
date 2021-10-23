9042a7390c8f88656ad6cc5de05d2c3de909fdf1
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-10-23 10:50:40 +0530

    updated

--- OPPS/exceptionahHandling/throwCatch.cpp
+++ OPPS/exceptionahHandling/throwCatch.cpp
@@ -7,22 +7,20 @@ using namespace std;
 int division(int a,int b)
 {
 	if(b==0)
-		throw 1;
+		throw 101;
 	return a/b;
 }	
 int main()
 {
-	int x=10,y=2,z;
+	int x=10,y=0,z;
 	try
 	{
-		if(y==0)
-			throw 1;
-		z=x/y;
-		cout<<z<<endl;
+	 z= division(x,y);
+	 cout<<z<<endl;
 	}
 	catch(int e)
 	{
-		cout<<"division by zero"<<e<<endl;
+		cout<<"division by zero "<<e<<endl;
 	}
 	cout<<"bye"<<endl;
 }
