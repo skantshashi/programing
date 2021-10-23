3d2bc9c625223c6a7c8591a60a66b9b7254a227c
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-10-23 11:25:02 +0530

    updated

--- OPPS/exceptionahHandling/allaboutThrow.cpp
+++ OPPS/exceptionahHandling/allaboutThrow.cpp
@@ -4,18 +4,25 @@ using namespace std;
 /*program for all about throw
 
 */
-class myexception: exception
+class myexception: exception //exception is builtin function in c++
 {
+	public:
+	int a=20;
+	void display()
+	{
+		cout<<a<<endl;
+	}
 };
-int division(int a,int b)throw(myexception)
+int division(int a,int b)throw(myexception)//
 {
 	if(b==0)
-		throw myexception();
+		throw myexception();//can throw anything like int, float, string, char, object of an class
 	return a/b;
 }
 int main()
 {
-	int x=10,y=2,z;
+	int x=10,y=0,z;
+	myexception my;
 	try
 	{
 		z=division(x,y);
@@ -23,7 +30,8 @@ int main()
 	}
 	catch(myexception e)
 	{
-		cout<<"division by zero"<<endl;
+		cout<<"division by zero "<<endl;
+		my.display();
 	}
 	cout<<"bye"<<endl;
 }
