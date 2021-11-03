007d8288863298509921ba945f3644cffae31736
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-04 02:49:39 +0530

    stl using map

--- STL/classUsingSTL.cpp
+++ STL/classUsingSTL.cpp
@@ -10,11 +10,12 @@ int main()
 	vector<int> v={2,4,6,8,10};
 	v.push_back(20);
 	v.push_back(31);
+		cout<<"using for each loop"<<" ";
+	for(int x:v)
+		cout<<x<<" ";
 	vector<int>::iterator itr;
 	cout<<"using iterator "<<endl;
 	for(itr=v.begin();itr!=v.end();itr++)
 		cout<<++*itr<<" ";
-	cout<<"using for each loop"<<" ";
-	for(int x:v)
-		cout<<x<<" ";
+
 }
