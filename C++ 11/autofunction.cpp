66734f9b43fa85f49390df36ff9044baea2368d5
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-07 11:58:06 +0530

    auto datatype

---
+++ C++ 11/autofunction.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*program for using auto function
+
+*/
+float fun()
+{
+	return 2.34f;
+}
+int main()
+{
+	double d=12.3f;
+	int i=9;
+	auto x=2*d+i;
+	cout<<x;
+}
