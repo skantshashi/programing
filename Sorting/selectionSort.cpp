d190ebc5da8938b81d374464f7f7bc9f602620fe
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-08 03:35:07 +0530

    update

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -24,14 +24,14 @@ void SelectionSort(int A[],int n)
 int main()
 {
  int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
-  for(i=0;i<10;i++)
+  
+for(i=0;i<10;i++)
  printf("%d ",A[i]);
  cout<<endl;
+ 
  SelectionSort(A,n);
  
  for(i=0;i<10;i++)
  cout<<A[i]<<" ";
-
- 
  return 0;
 }
