67d6e6c871d3bf6861e8b3d46d3657134711de9a
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-07 13:02:43 +0530

    update

---
+++ C++ 11/sharedPtr.cpp
@@ -0,0 +1,34 @@
+# include<iostream>
+# include<memory>
+using namespace std;
+    
+/*program for using stared pointer
+
+*/
+class rectangle
+{
+	int length;
+      	int breadth;
+public:
+	rectangle(int l,int b)
+	{
+		length=l;
+	      	breadth=b;
+	}
+     	int area()
+	{
+		return length*breadth;
+	}
+};
+int main()
+{
+	
+    shared_ptr<rectangle> ptr(new rectangle(15,60));
+	cout<<ptr->area()<<endl;
+	shared_ptr<rectangle> ptr2;
+	ptr2=ptr;
+	cout<<"ptr2 "<<ptr2->area()<<endl;
+	cout<<"ptr "<<ptr->area()<<endl;
+	cout<<ptr.use_count()<<endl;// shows no of pointer pointing to that object
+    
+}
