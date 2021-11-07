350e7426e57adb96fc32c93c74f7a370b27fb92d
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-07 12:57:35 +0530

    update

---
+++ C++ 11/uniquePtr.cpp
@@ -0,0 +1,31 @@
+# include<iostream>
+# include<memory>
+using namespace std;
+    
+/*program for using unique pointer
+
+*/
+class rectangle
+{
+	int length;
+      	int breadth;
+public:
+	rectangle(int l,int b)
+	{
+		length=l;
+	      	breadth=b;
+	}
+     	int area()
+	{
+		return length*breadth;
+	}
+};
+int main()
+{
+	unique_ptr<rectangle> ptr(new rectangle(10,5));
+	cout<<ptr->area()<<endl;
+	unique_ptr<rectangle> ptr2;
+	ptr2=move(ptr);
+	cout<<ptr2->area();
+	//cout<<ptr->area();   because ponter is moved
+}
